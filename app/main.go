30dfe817fe16c785b0c4a3f9e0fae39861d68c18
Samy30 <samibelaid30@gmail.com>
2021-01-22 16:40:42 +0100

    add create log file if not found functionality

--- app/main.go
+++ app/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	file, err := os.Open("logs/logs.txt")
+	file, err := os.OpenFile("logs/logs.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal("file not found")
 	}
